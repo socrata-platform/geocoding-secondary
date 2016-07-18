1e9b3a1d66bf02cb0e09f02f8cacb267a88377e0
Robert J. Macomber <robert.macomber@socrata.com>
2016-07-18 11:08:51 -0700

    Bump to latest data-coordinator

--- src/main/scala/com/socrata/geocodingsecondary/Main.scala
+++ src/main/scala/com/socrata/geocodingsecondary/Main.scala
@@ -3,9 +3,5 @@ package com.socrata.geocodingsecondary
 import com.socrata.datacoordinator.secondary.SecondaryWatcherApp
 
 object Main extends App {
-  val instance = Option(System.getenv("GEOCODING_SECONDARY_INSTANCE")).getOrElse {
-    System.err.println("GEOCODING_SECONDARY_INSTANCE environment variable not set")
-    sys.exit(1)
-  }
-  SecondaryWatcherApp(instance, new GeocodingSecondary(_))
+  SecondaryWatcherApp(new GeocodingSecondary(_))
 }
